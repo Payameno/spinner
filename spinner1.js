528596100c71d07930d28db9ade441dff8299a89
Payam Beigi <beigi.payam@gmail.com>
2022-06-07 22:02:03 +0000

    Dried code for spinner

--- spinner1.js
+++ spinner1.js
@@ -1,33 +1,8 @@
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 100);
-
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 300);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 500);
-
-setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
-}, 700);
-
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 900);
-
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 1100);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 1300);
-
-setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
-}, 1500);
+const spinChar = "| / - \\ | / - \\";
+let delay = 0;
+for (const char of spinChar) {
+  setTimeout(() => {
+    process.stdout.write(char + '\r');
+  }, delay);
+  delay += 200;
+};
